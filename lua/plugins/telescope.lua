e312cdb192595422e353bc891c6c536ed9ae2915
Hong Son Ngo <toilahongson@gmail.com>
2024-05-15 22:50:53 +0200

    LSP config from Kickstarter from DJ DeVries

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,8 +7,15 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'File grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find with grep' })
+        vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find keymaps' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find in help'})
+
+        vim.keymap.set('n', '<leader>fn',
+            function()
+                builtin.find_files { cwd = vim.fn.stdpath 'config' }
+            end,
+            { desc = 'Find Neovim files' })
+
     end
 }
