c52621b4673eddc9c744efba2961f311ce5fc3f2
Hong Son Ngo <toilahongson@gmail.com>
2024-05-16 20:23:21 +0200

    Reformat with stylua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,23 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            auto_install = true,
-            ensure_installed = {
-                "vimdoc",
-                "c",
-                "cpp",
-                "make",
-                "cmake",
-                "rust",
-                "lua",
-                "c_sharp",
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.install").compilers = { "clang", "gcc" }
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			ensure_installed = {
+				"vimdoc",
+				"c",
+				"cpp",
+				"make",
+				"cmake",
+				"rust",
+				"lua",
+				"c_sharp",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,27 +1,27 @@
 return { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    opts = {
-        preset = {
-            operators = true,
-            motions = true,
-            text_object = true,
-            windows = true,
-            nav = true,
-            z = true,
-            g = true,
-        },
-        operators = { gc = "Comments" },
-    },
-    config = function(_, opts) -- This is the function that runs, AFTER loading
-        require('which-key').setup(opts)
-        -- Document existing key chains
-        require('which-key').register({
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-            ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-            ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        })
-    end,
+	"folke/which-key.nvim",
+	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+	opts = {
+		preset = {
+			operators = true,
+			motions = true,
+			text_object = true,
+			windows = true,
+			nav = true,
+			z = true,
+			g = true,
+		},
+		operators = { gc = "Comments" },
+	},
+	config = function(_, opts) -- This is the function that runs, AFTER loading
+		require("which-key").setup(opts)
+		-- Document existing key chains
+		require("which-key").register({
+			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+		})
+	end,
 }
