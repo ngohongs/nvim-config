244c46c57e0961e5d94478ff35a8a7d3e70c6a2f
Hong Son Ngo <toilahongson@gmail.com>
2024-05-17 16:44:46 +0200

    adding space between line number and line

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -80,7 +80,7 @@ return {
 					sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
 					click = "v:lua.ScSa",
 				},
-				{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+				{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
 				{
 					sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
 					click = "v:lua.ScSa",
