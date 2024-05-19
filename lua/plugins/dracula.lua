e78afac1a03f393ce2fa8fee9f34b7b36507a5a0
Hong Son Ngo <toilahongson@gmail.com>
2024-05-19 16:41:10 +0200

    Gutter colorschemeA

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -8,9 +8,31 @@ return {
                 TroubleIndent = { bg = "NONE" },
                 TroubleFoldIcon = { bg = "NONE" },
                 -- TroubleText = { gui = "NONE" },
+                
+                LineNr = { fg = colors.comment, bg = colors.menu },
+                SignColumn = { bg = colors.menu },
+                FoldColumn = { fg = colors.comment, bg = colors.menu },
+                GitSignsAdd = { fg = colors.bright_green, bg = colors.menu },
+                GitSignsChange = { fg = colors.cyan, bg = colors.menu },
+                GitSignsDelete = { fg = colors.bright_red, bg = colors.menu },
+                DiagnosticSignError = { fg = colors.red, bg = colors.bg },
+                DiagnosticSignWarn = { fg = colors.yellow, bg = colors.bg },
+                DiagnosticSignInfo = { fg = colors.cyan, bg = colors.bg },
+                DiagnosticSignHint = { fg = colors.cyan, bg = colors.bg },
             }
+        })
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function()
+                vim.cmd("hi DiagnosticSignError guifg=" .. colors.red .. " guibg=" .. colors.menu)
+                vim.cmd("hi DiagnosticSignWarn guifg=" .. colors.yellow .. " guibg=" .. colors.menu)
+                vim.cmd("hi DiagnosticSignInfo guifg=" .. colors.cyan .. " guibg=" .. colors.menu)
+                vim.cmd("hi DiagnosticSignHint guifg=" .. colors.cyan .. " guibg=" .. colors.menu)
+            end
         })
 
         vim.cmd.colorscheme("dracula")
     end,
+    lazy = false,
+    priority = 1000,
 }
