d20651f50159778dc4fbbc707d0de941ac15656e
Hong Son Ngo <toilahongson@gmail.com>
2024-05-19 15:37:28 +0200

    Removing redundant hi groups

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -5,16 +5,13 @@ return {
         require("dracula").setup({
             overrides = {
                 TroubleNormal = { bg = colors.menu },
-                -- TroubleFile = { bg = colors.menu },
                 TroubleIndent = { bg = "NONE" },
                 TroubleFoldIcon = { bg = "NONE" },
-                -- TroubleCount = { bg = colors.menu },
                 -- TroubleText = { gui = "NONE" },
             }
         })
 
         -- vim.cmd[[hi TroubleText guibg=none]]
         vim.cmd.colorscheme("dracula")
-        vim.cmd[[highlight TroubleText guibg=none]]
     end,
 }
