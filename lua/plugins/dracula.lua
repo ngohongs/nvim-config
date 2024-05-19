90a6e6a74aada1ef24d8d37aa9f220407d18030e
Hong Son Ngo <toilahongson@gmail.com>
2024-05-19 11:48:03 +0200

    Fix dracula theme

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,23 +1,8 @@
 return {
     "Mofiqul/dracula.nvim",
-    name = "dracula",
-    priority = 1000,
     config = function()
         local colors = require("dracula").colors()
         require("dracula").setup({
-            transparent_bg = true,
-            overrides = {
-                NeoTreeNormal = { fg = colors.fg, bg = colors.menu, },
-                NeoTreeNormalNC = { fg = colors.fg, bg = colors.menu, },
-                SignColumn = { bg = colors.bg, },
-                FoldColumn = { bg = colors.bg, },
-                LineNr = { fg = colors.comment, bg = colors.bg },
-                TroubleNormal = { bg = colors.menu },
-                -- TroubleFile = { bg = colors.menu },
-                TroubleIndent = { bg = colors.menu },
-                TroubleFoldIcon = { bg = colors.menu },
-                -- TroubleCount = { bg = colors.menu },
-            },
         })
         vim.cmd.colorscheme("dracula")
     end,
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     },
     config = function()
         require("lualine").setup({
-            theme = "dracula",
+            theme = "dracula-nvim",
         })
     end,
 }
